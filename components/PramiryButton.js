088489c4fef0c33777157080281a198e67f9265e
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-02-17 13:47:43 +0400

    creatung custom btn

---
+++ components/PramiryButton.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const PramiryButton = ({ children }) => {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+};
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,12 @@
 import { TextInput, View } from "react-native";
+import { PramiryButton } from "../components/PramiryButton";
 
 export const StartGameScreen = () => {
   return (
     <View>
       <TextInput />
-      BUTTONS
+      <PramiryButton>Reset</PramiryButton>
+      <PramiryButton>Confirm</PramiryButton>
     </View>
   );
 };
