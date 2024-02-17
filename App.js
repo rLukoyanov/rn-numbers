d075d876d852789ca6c0cef885a0a5df81d854ba
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-02-17 13:59:51 +0400

    styling for ios and android

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { StartGameScreen } from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <View>
-      <StartGameScreen />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,27 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import { PramiryButton } from "../components/PramiryButton";
 
 export const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PramiryButton>Reset</PramiryButton>
       <PramiryButton>Confirm</PramiryButton>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    borderRadius: 9,
+    elevation: 4,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 5 },
+    shadowRadius: 6,
+    shadowOpacity: 0.5,
+  },
+});
