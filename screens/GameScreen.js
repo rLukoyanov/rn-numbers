7933edf3922e567db32b8190b0279b647fd263b1
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-02-17 13:41:26 +0400

    setting up screens

---
+++ screens/GameScreen.js
@@ -0,0 +1,3 @@
+export const GameScreen = () => {
+  return <div>GameScreen</div>;
+};
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,10 @@
+import { TextInput, View } from "react-native";
+
+export const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      BUTTONS
+    </View>
+  );
+};
