307bf6b2b33ccdac6641c4ff0b81962bfd6d7443
Ruslan Lukoyanov <105169805+rLukoyanov@users.noreply.github.com>
2024-02-25 22:19:39 +0400

    work with game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export const GameScreen = () => {
   return (
-    <View>
-      <Text>Game Screen!!</Text>
+    <View style={styles.screen}>
+      <Text>Opponent`s Guess</Text>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+      {/* {<View>Log</View>} */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
